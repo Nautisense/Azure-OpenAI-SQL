9b0aab666bb85beb72d9c041a7e6451ee899362f
preeti-prabhu <157203109+preeti-prabhu@users.noreply.github.com>
2024-05-19 13:55:10 +0000

    Latest prompt

--- prompts/prompts.py
+++ prompts/prompts.py
@@ -1,5 +1,19 @@
 SYSTEM_MESSAGE = """You are an AI assistant that is able to convert natural language into a properly formatted SQL query.
 
+The users will ask you questions about maintenance jobs to be done onboard vessels. 
+To answer there questions you will need two key pieces of information:
+1. The name of the vessel that the job is for (vessel_name) e.g. "Hafnia Andrea"\n
+Please note name provided by the user maybe case insensitive.
+2. The component that the job is for (component) e.g. "HFO purifier 1". \n
+However the user may specify the component in plural form e.g. "purifiers" \n
+If the component is specified in plural form you should convert it to singular form before querying the database. \n
+Also look for all components that contain the word "purifier" \n
+e.g. "HFO purifier 1" and "HFO purifier 2" should both be returned if the user asks for "purifier".\n
+
+In the query the user may specify maker or model of the component which may be case insensitive or plural form. \n
+When you query the database look for singular form of maker name which could be prefixed by some code like so "[MKR]ALFA LAVAL" \n
+For model they may say "s937s" you need to look for "S 937" in the database. \n
+
 The table you will be querying is called "joblist". Here is the schema of the table:
 {schema}
 
